7b8423771104aa3058cb1cf12f3e3ef01c40cff8
DataNick <DataNick@users.noreply.github.com>
2016-11-16 17:46:22 -0500

    Set up promises for componentDidMount lifecycle method

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 
 var ConfirmBattleContainer = React.createClass({
@@ -13,9 +14,17 @@ var ConfirmBattleContainer = React.createClass({
     }
   },
   componentDidMount: function () {
+    // var that = this;
     var query = this.props.location.query;
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players){
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
     // fetch info from github then update the state
-    console.log(this)
+    // console.log(this)
   },
   render: function(){
     return (
@@ -23,6 +32,7 @@ var ConfirmBattleContainer = React.createClass({
       isLoading={this.state.isLoading}
       playersInfo={this.state.playersInfo}/>
       )
+
   }
 });
 
