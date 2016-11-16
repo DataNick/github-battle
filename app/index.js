0f7b28ebb34c4fcc28fd49b2ede418bb5239a71c
DataNick <DataNick@users.noreply.github.com>
2016-11-16 00:47:25 -0500

    Modify index.js

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+/*
+  Focused
+  Independent
+  Reusable
+  Small
+  Testable
+*/
 
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <div>Hello ReactJS Program</div>
-      )
-  }
-});
-
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
